7a498ea42a13fb5c46436ee03c8bdb611e358a8d
Paul <you@example.com>
2020-12-07 15:34:24 +0100

    got the data, looks like dog-doo-doo tho :/

--- js/list.js
+++ js/list.js
@@ -3,22 +3,39 @@ getToilets()
 // Asynchronous function that enables waiting
 async function getToilets() {
     // loading the data, followind instructions will wait until loading is finished
-    let response = await fetch("https://kloapp.herokuapp.com/all")
+    let response = await fetch("https://kloapp.herokuapp.com/all");
 
     if (response.ok) {
         // converting from json to array, will wait until conversion is finished
-        let list = await response.json()
+        let list = await response.json();
 
         // get reference to <div id="list">
-        const listDIV = document.getElementById("list")
-        
-        // add your code here
-        const html = `<div class="card"><h2>test</h2></div>`
+        const listDIV = document.getElementById("list");
 
+        // add your code here
+        console.log(list);
+        let html = "";
+        for (let i = 0; i < list.length; i++) {
+            html += `
+            <div class="container-fluid">
+            <div class="row row-tols-3 p-3">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                                <h5 class="card-title">${list[i].city}</h5>
+                                    <p class="card-text">${list[i].street} ${list[i].name}.</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        `
+        }
         // insert your html into referenced div
-        listDIV.innerHTML = html
+        listDIV.innerHTML = html;
 
     } else {
-        alert("List loading Error: " + response.status + " " + response.statusText + " " + response.url)
+        alert("List loading Error: " + response.status + " " + response.statusText + " " + response.url);
     }
 }
