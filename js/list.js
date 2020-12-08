b65cff699687b470ab579350a854e48b2c933713
Nour-jin <69240670+Nour-jin@users.noreply.github.com>
2020-12-08 11:28:10 +0100

    New-update-WelcomeJs

--- js/list.js
+++ js/list.js
@@ -1,68 +1,63 @@
 getToilets()
+
 // Asynchronous function that enables waiting
 async function getToilets() {
     // loading the data, followind instructions will wait until loading is finished
     let response = await fetch("https://kloapp.herokuapp.com/all")
+
     if (response.ok) {
         // converting from json to array, will wait until conversion is finished
         let list = await response.json()
         console.log(list)
         // get reference to <div id="list">
         const listDIV = document.getElementById("list")
-        // add your code here
-        const html = `<div class="card"><h2>test</h2></div>`
-        newList(list)
-        function newList(data){
-            for (let i = 0; i < data.length; i++) {
-                const toilet = data[i]
-                const {
-                    name,
-                    city,
-                    zip,
-                    street,
-                    streetnr:nr,
-                    stalls,
-                    ['feat-access']:access,
-                    rating,
-                    ...features
-                } = toilet
 
-                let featString = ""
-                
-            for(feat in features) {
-                feat = feat.split("-")[1]
-                featString += `<div class="card">${feat[0].toUpperCase()}${feat.substr(1)}</div>`
-            }   
-            
-            var newData = `<div class="col" ontouchstart="this.classList.toggle('hover');">
-            <div class="container">
-                <div class="front" style="background-image: url(https://unsplash.it/500/500/)">
-                    <div class="inner">
-                        <p>${city}</p>
-          <span>Hover For InFo</span>
-                    </div>
-                </div>
-                <div class="back">
-                    <div class="inner">
-                    <div class="card">Zip:  ${zip}</div>
-                    <div class="card">Address : ${street} </div>
-                    <div class="card">Street Nur: ${nr}</div>
-                    <div class="card">Location : ${name}</div> 
-                    <div class="card">Access : ${access}</div>
-                    <div class="card">Stalls : ${stalls}</div>
-                    ${featString}
-                    <div class="card">Rating : ${rating}</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-		`
-            listDIV.innerHTML += newData
-        
-            }
-        }  
-        // insert your html into referenced div
+        newList(list, listDIV)
     } else {
         alert("List loading Error: " + response.status + " " + response.statusText + " " + response.url)
     }
 }
+
+function newList(data, listDIV){
+    for (let index = 0; index < data.length; index++) {
+        const toilet = data[index]                
+
+        listDIV.innerHTML += entry(toilet)
+        
+        console.log(data[index])
+    }
+}
+
+function entry({name, city, zip, street, streetnr:nr, stalls, ['feat-access']:access, rating, ...features}){
+    let featString = ""
+
+    for(feat in features) {
+        feat = feat.split("-")[1]
+        featString += `<div class="card">${feat[0].toUpperCase()}${feat.substr(1)}</div>`
+    }
+    
+    return `<div class="col" ontouchstart="this.classList.toggle('hover');">
+    <div class="container">
+        <div class="front" style="background-image: url(https://unsplash.it/500/500/)">
+            <div class="inner">
+                <p>${city}</p>
+  <span>Hover For InFo</span>
+            </div>
+        </div>
+        <div class="back">
+            <div class="inner">
+            <div class="card">Zip:  ${zip}</div>
+            <div class="card">Address : ${street} </div>
+            <div class="card">Street Nur: ${nr}</div>
+            <div class="card">Location : ${name}</div> 
+            <div class="card">Access : ${access}</div>
+            <div class="card">Stalls : ${stalls}</div>
+            ${featString}
+            <div class="card">Rating : ${rating}</div>
+            </div>
+        </div>
+    </div>
+</div>`
+} 
+            
+         
