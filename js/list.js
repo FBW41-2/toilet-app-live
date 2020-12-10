e33c895c489d5b86300db603b9d97eb0012552d8
galymax <werkhowski@gmail.com>
2020-12-10 10:34:17 +0100

    map in list

--- js/list.js
+++ js/list.js
@@ -19,27 +19,28 @@ async function getToilets() {
 }
 
 function newList(data, listDIV){
-    for (let index = 0; index < data.length; index++) {
-        const toilet = data[index]                
-
-        listDIV.innerHTML += entry(toilet)
-        
-        console.log(data[index])
+    const htmlEmelntsArray = data.map(entry)
+    listDIV.innerHTML = htmlEmelntsArray.join("")
+    /* internal workings of map
+    const newArr = []
+    for(i = 0; i < data.length; i++) {
+        newArr.push(callback(data[i]))
     }
+    return newArr
+    */
 }
 
 function entry({name, city, zip, street, streetnr:nr, stalls, ['feat-access']:access, rating, ...features}){
-    let featString = ""
-
-    for(feat in features) {
+    const featureNames = Object.keys(features)
+    let featString = featureNames.map(feat => {
         feat = feat.split("-")[1]
-        featString += `<div class="card"><h2>${feat[0].toUpperCase()}${feat.substr(1)}</h2></div>`
-    }
+        return `<h2>${feat[0].toUpperCase()}${feat.substr(1)}</h2>`
+    }).join("")
     
-    return `<div class="card"><h2>Location: ${name}</h2></div> 
-    <div class="card"><h2>City: ${city} ${zip}</h2></div>
-    <div class="card"><h2>Address: ${street} ${nr}</h2></div>
-    <div class="card"><h2>Stalls: ${stalls}</h2></div>
-    <div class="card"><h2>Access: ${access}</h2></div>
-    ${featString}`
+    return `<div class="card"><h2>Location: ${name}</h2> 
+    <h2>City: ${city} ${zip}</h2>
+    <h2>Address: ${street} ${nr}</h2>
+    <h2>Stalls: ${stalls}</h2>
+    <h2>Access: ${access}</h2>
+    ${featString}</div>`
 }
